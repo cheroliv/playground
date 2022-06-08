3508c4fa756953ee8297d7cdcafe5a767c808231
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-09 00:06:29 +0200

    add kata

--- src/test/kotlin/playground/programming/FirstProgramTest.kt
+++ src/test/kotlin/playground/programming/FirstProgramTest.kt
@@ -2,8 +2,10 @@ package playground.programming
 
 import kotlin.test.Test
 
-class FirstProgramTest{
-   @Test fun simple_test() {
+class FirstProgramTest {
+
+    @Test
+    fun simple_test() {
         println("Write your first program in kotlin!")
         println("no line break")
         println("")
