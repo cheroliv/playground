acdf1a8b91ba92ec00d224ee4922f371efcab14e
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-08 17:42:19 +0200

    add kata

---
+++ src/test/kotlin/playground/programming/ParseIntCharProblemTest.kt
@@ -0,0 +1,18 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
+package playground.programming
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ParseIntCharProblemTest {
+
+    fun getAge(y: String): Int = y.filter {
+        it.isDigit()
+    }.toInt()
+
+    @Test
+    fun `deviner l'age contenu au debut de la chaine de caractere`(): Unit {
+        assertEquals(52,getAge("52 kkkkkkk"))
+    }
+}
