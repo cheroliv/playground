b39c72f723cdd5faab592e0c82774fb08b4940aa
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-16 23:57:02 +0200

    update StringsTest.kt

--- src/test/kotlin/playground/programming/StringsTest.kt
+++ src/test/kotlin/playground/programming/StringsTest.kt
@@ -1,12 +1,20 @@
+@file:Suppress(
+    "NonAsciiCharacters",
+    "ConvertToStringTemplate",
+    "ReplaceGetOrSet",
+    "PLATFORM_CLASS_MAPPED_TO_KOTLIN",
+    "KotlinConstantConditions",
+    "ReplaceCallWithBinaryOperator",
+)
+
 package playground.programming
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class StringsTest {
-    @Suppress("NonAsciiCharacters")
     @Test
     fun `chaines de caractères et caractères`() {
         //inférence de type
@@ -14,32 +22,31 @@ class StringsTest {
         assertEquals("C'est", s)
 
         //concaténation
-        @Suppress("ConvertToStringTemplate")
-        val t: String = s + " le moment"
-        assertEquals("C'est le moment", t)
+        val t: String = s + " le moment."
+        assertEquals("C'est le moment.", t)
 
         // StringBuilder
-        val t1: String = s + buildString { append(" le moment") }
-        assertEquals("C'est le moment", t1)
+        val t1: String = s + buildString { append(" le moment.") }
+        assertEquals("C'est le moment.", t1)
 
         //interpolation
-        val t2 = "$s le moment"
-        assertEquals("C'est le moment", t2)
+        val t2 = "$s le moment."
+        assertEquals("C'est le moment.", t2)
 
         //multiligne chaine de caractères
-        val t3 = """$s le moment"""
-        assertEquals("C'est le moment", t3)
+        val t3 = """$s le moment."""
+        assertEquals("C'est le moment.", t3)
 
         //conversion de type
         @Suppress("ConvertToStringTemplate")
         val t4 = t + " " + 23.4
-        assertEquals("C'est le moment 23.4", t4)
+        assertEquals("C'est le moment. 23.4", t4)
 
         val t5 = 'C'.toString()
         assertEquals("C", t5)
 
         //taille de la chaine de caractères
-        assertEquals(15, t.length)
+        assertEquals(16, t.length)
 
         //sous-chaine d'une chaine de caractères
 
@@ -47,7 +54,7 @@ class StringsTest {
         //les caractères aux positions x à y-1
         //sub = t.substring(x, y)
 
-        //t = "C'est le moment"
+        //t = "C'est le moment."
         //retourne les caractères 6 et 7
         var sub = t.substring(6, 8)
         assertEquals("le", sub)
@@ -61,7 +68,6 @@ class StringsTest {
         assertEquals(5 - 0, numChars)
 
         //extraction des caractères d'une chaine
-        @Suppress("ReplaceGetOrSet")
         assertEquals('e', t.elementAt(2))
         assertEquals('e', t.get(2))
         assertEquals('e', t[2])
@@ -73,26 +79,34 @@ class StringsTest {
 
         //place les 4 premiers caractères de t1
         //dans le tableau ca a la position 2
-        @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
-        (t as java.lang.String).getChars(0, 3, ca, 1)
-        assertEquals("CC'et le moment", String(ca))
-        assertEquals("CC'et le moment", ca.concatToString())
+        (t as java.lang.String).getChars(
+            /* srcBegin = */ 0,
+            /* srcEnd = */ 3,
+            /* dst = */ ca,
+            /* dstBegin = */ 1
+        )
+        assertEquals("CC'et le moment.", String(ca))
+        assertEquals("CC'et le moment.", ca.concatToString())
 
         //to lower case
-        assertEquals("c'est le moment", t.toLowerCase())
-        assertEquals("c'est le moment", t.lowercase())
+        assertEquals("c'est le moment.", t.toLowerCase())
+        assertEquals("c'est le moment.", t.lowercase())
         //to lower case
-        @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
-        assertEquals("C'EST LE MOMENT", t.toUpperCase())
-        assertEquals("C'EST LE MOMENT", t.uppercase())
+        assertEquals("C'EST LE MOMENT.", t.toUpperCase())
+        assertEquals("C'EST LE MOMENT.", t.uppercase())
 
         //comparaison de chaines de caractères
-        //t = "C'est le moment"
-        asserFalse(t.equals("hello"))
-        asserFalse(t == "hello")
-        assertTrue(t.equalsIgnoreCase("C'EST LE MOMENT"))
-        assertTrue(t.equals("C'EST LE MOMENT", ignoreCase = true))
+        //t = "C'est le moment."
+        assertFalse(t.equals("hello"))
+        assertFalse(t == "hello")
+        //ignore la casse
+        assertTrue(t.equalsIgnoreCase("C'EST LE MOMENT."))
+        assertTrue(t.equals("C'EST LE MOMENT.", ignoreCase = true))
 
+        //démarre par
+        assertTrue(t.startsWith("C'est"))
+        //se finit par
+        assertTrue(t.endsWith("le moment."))
 
 //        println(t.toUpperCase())
     }
