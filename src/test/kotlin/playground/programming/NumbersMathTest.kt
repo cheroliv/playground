3feef47458c637686487109e9df80b5b615e58e4
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-17 23:44:45 +0200

    update NumbersMathTest.kt

--- src/test/kotlin/playground/programming/NumbersMathTest.kt
+++ src/test/kotlin/playground/programming/NumbersMathTest.kt
@@ -5,6 +5,28 @@ import kotlin.test.Test
 class NumbersMathTest {
     @Test
     fun `Nombres et Math`() {
+        //Constantes utiles
+        Byte.MIN_VALUE
+        Byte.MAX_VALUE
+        Short.MIN_VALUE
+        Short.MAX_VALUE
+        Float.MIN_VALUE
+        Float.MAX_VALUE
+        Math.PI
+        Math.E
+        val s = "-42"
+        //conversion de chaine de caractères
+        //vers un nombre, si possible.
+        val b: Byte = java.lang.Byte.parseByte(s)
+        val sh: Short = java.lang.Short.parseShort(s)
+        val i: Int = java.lang.Integer.parseInt(s)
+        val l: Long = java.lang.Long.parseLong(s)
+        val f: Float = java.lang.Float.parseFloat(s)
+        val d: Double = java.lang.Double.parseDouble(s)
+
+        //valeur exacte
+        val f_exac = java.lang.Float.valueOf(s)
+        val d_exac = java.lang.Double.valueOf(s)
 
     }
 }
