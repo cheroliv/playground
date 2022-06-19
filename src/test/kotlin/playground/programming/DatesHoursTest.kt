3379918ec916768e8dc26edded6c01cb1d62a88d
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-19 22:55:17 +0200

    update DatesHoursTest.kt

--- src/test/kotlin/playground/programming/DatesHoursTest.kt
+++ src/test/kotlin/playground/programming/DatesHoursTest.kt
@@ -1,10 +1,28 @@
 package playground.programming
 
+import java.text.DateFormat
+import java.util.*
 import kotlin.test.Test
 
 class DatesHoursTest {
+
     @Test
     fun `Dates et heures`() {
 
+        //l'heure courante en millisecondes
+        val t0 = System.currentTimeMillis()
+        //une autre repsentation de la meme information
+        val now = java.util.Date()
+        //converti un objet java.util.Date en une valeur long.
+        @Suppress("UsePropertyAccessSyntax")
+        val t1 = now.getTime()
+        //kotlin property access syntaxe
+        @Suppress("UNUSED_VARIABLE", "LocalVariableName")
+        val t1_prime = now.time
+
+        //affiche la date d'aujourd'hui en utilisant le format
+        //par défaut des parametres locaux
+        val dateFormat = DateFormat.getDateInstance(1, Locale.FRANCE)
+        println(dateFormat.format(Date()))
     }
 }
