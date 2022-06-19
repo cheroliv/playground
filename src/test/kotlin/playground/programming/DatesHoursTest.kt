77313a9848268a19e435472d58f72b1bb264b427
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-19 23:20:23 +0200

    update DatesHoursTest.kt

--- src/test/kotlin/playground/programming/DatesHoursTest.kt
+++ src/test/kotlin/playground/programming/DatesHoursTest.kt
@@ -1,8 +1,11 @@
 package playground.programming
 
 import java.text.DateFormat
+import java.time.Instant
 import java.util.*
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class DatesHoursTest {
 
@@ -12,17 +15,29 @@ class DatesHoursTest {
         //l'heure courante en millisecondes
         val t0 = System.currentTimeMillis()
         //une autre repsentation de la meme information
+        @Suppress("RemoveRedundantQualifierName")
         val now = java.util.Date()
         //converti un objet java.util.Date en une valeur long.
         @Suppress("UsePropertyAccessSyntax")
         val t1 = now.getTime()
-        //kotlin property access syntaxe
+        assertTrue(t1 > Instant.EPOCH.toEpochMilli())
+        //kotlin property access syntaxe style
         @Suppress("UNUSED_VARIABLE", "LocalVariableName")
         val t1_prime = now.time
 
+        //java.text.DateFormat
         //affiche la date d'aujourd'hui en utilisant le format
         //par défaut des parametres locaux
-        val dateFormat = DateFormat.getDateInstance(1, Locale.FRANCE)
-        println(dateFormat.format(Date()))
+        val dateFormat = DateFormat.getDateInstance(DateFormat.LONG, Locale.FRANCE)
+        val localeFormattedDate = dateFormat.format(Date())
+
+        //constantes pour les styles de pattern de mise en forme
+        assertEquals(0,DateFormat.FULL)
+        assertEquals(1,DateFormat.LONG)
+        assertEquals(2,DateFormat.MEDIUM)
+        assertEquals(3,DateFormat.SHORT)
+        assertEquals(2,DateFormat.DEFAULT)
+
+//        println(dateFormat.format(Date(2019,5,7)))
     }
 }
