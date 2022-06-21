0b356457aa1d0a4d9658d1bcde622b8ca133e8a7
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-22 00:49:00 +0200

    update DatesHoursTest.kt

--- src/test/kotlin/playground/programming/ArrayCollectionStreamTest.kt
+++ src/test/kotlin/playground/programming/ArrayCollectionStreamTest.kt
@@ -22,8 +22,23 @@ class ArrayCollectionStreamTest {
         val pos = Arrays.binarySearch(intarray, 7)
         //la valeur 7 est trouvé a l'index 2
         assertEquals(2, pos)
-        //pas trouvé retourne une valeur negative
+        //12 pas trouvé retourne une valeur negative
         assert(Arrays.binarySearch(intarray, 12) < 0)
+
+        //les tableaux peuvent également etre triés
+        //et faire l'objet d'une recherche
+        val strarray = arrayOf("le", "moment", "c'est")
+        assertEquals("c'est", strarray[2])
+        assertEquals("le", strarray[0])
+        assertEquals("moment", strarray[1])
+        Arrays.sort(strarray)
+        assertEquals("c'est", strarray[0])
+        assertEquals("le", strarray[1])
+        assertEquals("moment", strarray[2])
+
+        //Arrays.equals() compare tous les éléments de deux tableaux
+        //Arrays.clone() copie tous les elements du tableau dans un autre
+        strarray.forEachIndexed { i, it -> assertEquals(it, strarray.clone()[i]) }
     }
 
     @Test
