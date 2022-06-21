84fea20b0b89f067f199c35905c9cb5c9e17a858
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-22 01:09:39 +0200

    update DatesHoursTest.kt

--- src/test/kotlin/playground/programming/ArrayCollectionStreamTest.kt
+++ src/test/kotlin/playground/programming/ArrayCollectionStreamTest.kt
@@ -10,47 +10,54 @@ import java.util.*
 import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class ArrayCollectionStreamTest {
     @Test
-    fun `Tableaux`() {
+    fun `Tableaux, collections, streams`() {
+        //Tableau
         //java.util.Arrays définit d'utiles méthodes de manipulation de tableaux, 
         //y compris de tri et de recherche au sein d'un tableau
-        val intarray = arrayOf(10, 5, 7, -3)
+        val intArray = arrayOf(10, 5, 7, -3)
         //tri le tableau
-        Arrays.sort(intarray)
-        val pos = Arrays.binarySearch(intarray, 7)
+        Arrays.sort(intArray)
+        val pos = Arrays.binarySearch(intArray, 7)
         //la valeur 7 est trouvé a l'index 2
         assertEquals(2, pos)
         //12 pas trouvé retourne une valeur negative
-        assert(Arrays.binarySearch(intarray, 12) < 0)
+        assert(Arrays.binarySearch(intArray, 12) < 0)
 
         //les tableaux peuvent également etre triés
         //et faire l'objet d'une recherche
-        val strarray = arrayOf("le", "moment", "c'est")
-        assertEquals("c'est", strarray[2])
-        assertEquals("le", strarray[0])
-        assertEquals("moment", strarray[1])
-        Arrays.sort(strarray)
-        assertEquals("c'est", strarray[0])
-        assertEquals("le", strarray[1])
-        assertEquals("moment", strarray[2])
+        val stringArray = arrayOf("le", "moment", "c'est")
+        assertEquals("c'est", stringArray[2])
+        assertEquals("le", stringArray[0])
+        assertEquals("moment", stringArray[1])
+        Arrays.sort(stringArray)
+        assertEquals("c'est", stringArray[0])
+        assertEquals("le", stringArray[1])
+        assertEquals("moment", stringArray[2])
 
         //Arrays.equals() compare tous les éléments de deux tableaux
         //Arrays.clone() copie tous les elements du tableau dans un autre
-        strarray.forEachIndexed { i, it -> assertEquals(it, strarray.clone()[i]) }
+        stringArray.forEachIndexed { i, it -> assertEquals(it, stringArray.clone()[i]) }
 
-        //Arrays.fill() initialise tous les éléments des deux tableaux
         val data = ByteArray(100)
-    }
+        //Arrays.fill() initialise tous les éléments des deux tableaux
+        //initalise tous les éléments à -1
+        Arrays.fill(data, -1)
+        data.forEach { assertEquals(-1, it) }
 
-    @Test
-    @Ignore
-    fun `Collections`() {
-    }
+        //attribue aux éléments 5, 6, 7, 8 et 9 la valeur -2
+        Arrays.fill(data, 5, 10, -2)
+        ((5 until (10 - 1))).forEach { assertEquals(-2, data[it]) }
+        //recupere le type de data
+        val type = data::class.java
+        //est ce que data est un tableau?
+        assertTrue(type.isArray())
+        //est ce que data est un tableau de byte
+        assertEquals(Byte::class.java, type.getComponentType())
 
-    @Test
-    @Ignore
-    fun `Stream`() {
+        //Collection
     }
 }
