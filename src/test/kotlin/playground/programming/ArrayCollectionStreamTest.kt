88b358df27b03fbc2e466af98a591ee6e51bd599
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-21 18:39:43 +0200

    update DatesHoursTest.kt

---
+++ src/test/kotlin/playground/programming/ArrayCollectionStreamTest.kt
@@ -0,0 +1,35 @@
+@file:Suppress(
+    "RemoveRedundantQualifierName",
+    "UsePropertyAccessSyntax",
+    "UNUSED_VARIABLE", "LocalVariableName",
+)
+
+package playground.programming
+
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class ArrayCollectionStreamTest {
+    @Test
+    fun `Tableaux`() {
+        //java.util.Arrays définit d'utiles méthodes de manipulation de tableaux, 
+        //y compris de tri et de recherche au sein d'un tableau
+        //construction d'un tableau d'entier
+        val intarray = arrayOf(10, 5, 7, -3)
+    }
+
+    @Test
+    @Ignored
+    fun `Collections`() {
+    }
+
+    @Test
+    @Ignored
+    fun `Stream`() {
+    }
+}
--- src/test/kotlin/playground/programming/DatesHoursTest.kt
+++ src/test/kotlin/playground/programming/DatesHoursTest.kt
@@ -1,12 +1,13 @@
-//@file:Suppress(
-//    "RemoveRedundantQualifierName",
-//    "UsePropertyAccessSyntax",
-//    "UNUSED_VARIABLE", "LocalVariableName",
-//)
+@file:Suppress(
+    "RemoveRedundantQualifierName",
+    "UsePropertyAccessSyntax",
+    "UNUSED_VARIABLE", "LocalVariableName",
+)
 
 package playground.programming
 
 import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.time.Instant
 import java.util.*
 import kotlin.test.Test
@@ -14,13 +15,11 @@ import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class DatesHoursTest {
-
     @Test
     fun `Dates et heures`() {
-
         //l'heure courante en millisecondes
         val t0 = System.currentTimeMillis()
-        //une autre repsentation de la meme information
+        //une autre représentation de la meme information
         val now = java.util.Date()
         //converti un objet java.util.Date en une valeur long.
         val t1 = now.getTime()
@@ -43,16 +42,68 @@ class DatesHoursTest {
         assertEquals(3, DateFormat.SHORT)
         assertEquals(2, DateFormat.DEFAULT)
 
-        //utilise pour l'heure un format abrégé
-        //en fonction de la locale
+        //utilise pour l'heure un format abrégé avec
+        //des parametres personnalisés
         val tf = DateFormat.getTimeInstance(
             DateFormat.SHORT,
             Locale.FRANCE
         )
+        //affiche l'heure en utilisant le format de tf
         val shortTime = tf.format(Date())
         assertTrue(shortTime.contains(':'))
 
+        //affiche la date et l'heure en utilisant
+        //un format détaillé
+        val longTimeStamp = DateFormat.getDateTimeInstance(
+            DateFormat.FULL,
+            DateFormat.FULL,
+        )
+        assert(longTimeStamp.format(Date()).isNotEmpty())
 
-//        println(dateFormat.format(Date(2019,5,7)))
+        //utilisez java.text.SimpleDateFormat
+        //pour définir votre propre modele de formatage
+        val customFormat = SimpleDateFormat("yyyy.MM.dd")
+        assertEquals(10, customFormat.format(Date()).length)
+
+        //DateFormat peut également parser les date contenu dans des chaines
+        val kotlinAnnounceDate = customFormat.parse("2019.05.08")
+
+        //la class Date et sa représentation en millisecondes
+        //n'autorise qu'une forme trés simple d'arithmétique
+        //on ajoute 3 600 000 millisecondes à l'heure courante
+        val anHourFromNow = now.getTime() + (60 * 60 * 1000)
+        assert(anHourFromNow > now.getTime())
+
+        //java.util.Calendar
+        //pour manipuler les dates et heures de facon plus sophistiquée
+        //instanciation selon les parametres locaux
+        //et le fuseau horaire local
+        val calendar = Calendar.getInstance()
+        //initialisation du calendrier à la date de maintenant
+        calendar.setTime(now)
+        //détermine le jour de l'année auquel correspond la date courante
+        val dayOfYear = calendar.get(Calendar.DAY_OF_YEAR)
+        assert(dayOfYear < 366)
+        //réinitialisation de la date courante
+        calendar.set(2019, Calendar.MAY, 8)
+        assertEquals(4, calendar.get(Calendar.DAY_OF_WEEK))
+
+        //à quel jour du mois correspond le deuxieme mercredi de mai 2019
+        //set(key,value)
+        calendar.set(Calendar.YEAR, 2019)
+        calendar.set(Calendar.MONTH, Calendar.MAY)
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.WEDNESDAY)
+        //defini à quel (n=2) semaine du mois est la date
+        calendar.set(Calendar.DAY_OF_WEEK_IN_MONTH, 2)
+        //extrait le jour du mois
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+        assertEquals(8, dayOfMonth)
+
+        calendar.setTime(kotlinAnnounceDate)
+        //ajoute 30j à la date
+        calendar.add(Calendar.DATE, 30)
+        val monthAfter = calendar.getTime()
+        //date est elle avant ou apres?
+        assertTrue(monthAfter.after(kotlinAnnounceDate))
     }
 }
