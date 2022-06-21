01014c15ec750e6719bd51eab0c23a60c31d9c74
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-21 18:52:01 +0200

    update DatesHoursTest.kt

--- src/test/kotlin/playground/programming/ArrayCollectionStreamTest.kt
+++ src/test/kotlin/playground/programming/ArrayCollectionStreamTest.kt
@@ -6,22 +6,24 @@
 
 package playground.programming
 
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.time.Instant
 import java.util.*
 import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 class ArrayCollectionStreamTest {
     @Test
     fun `Tableaux`() {
         //java.util.Arrays définit d'utiles méthodes de manipulation de tableaux, 
         //y compris de tri et de recherche au sein d'un tableau
-        //construction d'un tableau d'entier
         val intarray = arrayOf(10, 5, 7, -3)
+        //tri le tableau
+        Arrays.sort(intarray)
+        val pos = Arrays.binarySearch(intarray, 7)
+        //la valeur 7 est trouvé a l'index 2
+        assertEquals(2, pos)
+        //pas trouvé retourne une valeur negative
+        assert(Arrays.binarySearch(intarray, 12) < 0)
     }
 
     @Test
